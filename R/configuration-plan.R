cd483bf06b7e2d81c8d021036274ff7b52d1572d
Unknown <chelle.pierre@gmail.com>
2019-11-25 02:55:32 -0500

    Document class

--- R/configuration-plan.R
+++ R/configuration-plan.R
@@ -1,6 +1,14 @@
 #' @title ConfigurationPlan
 #' @docType class
-#' @description  Abstract class for ConfigurationPlan
+#' @description  R6 class for ConfigurationPlan
+#' @field reInputPath Input path to run the Configuration Plan
+#' @field reOutputPath Output path where Configuration Plan results will be stored
+#' @field simulationMapping R6 class mapping simulations to needed for plots
+#' @field observedDataSets R6 class storing the observed data
+#' @field plots R6 class defining the plots to be performed in Configuration Plan
+#' @field inputs Inputs to be moved as is in Configuration Plan output
+#' @field sections Description of output structure tree as a data.frame
+#' @field intro Introduction to be moved as is in Configuration Plan output
 #' @export
 ConfigurationPlan <- R6::R6Class(
   "ConfigurationPlan",
@@ -26,18 +34,10 @@ ConfigurationPlan <- R6::R6Class(
       self$reInputPath <- reInputPath
       self$reOutputPath <- reOutputPath
 
-      if (dir.exists(reOutputPath)) {
-        warning(paste("Reporting Engine output path:", reOutputPath, "already exists"),
-          call. = FALSE
-        )
-        if (overwriteREOutputPath) {
-          warning("Overwrite option is TRUE, files within path were deleted",
-            call. = FALSE
-          )
-          unlink(reOutputPath, recursive = TRUE)
-        }
-      }
-      # Warning already accounted in previous loop
+      checkOverwriteExisitingPath(reOutputPath)
+
+      # Warning already accounted for by previous check
+      # No need to show warnings twice
       dir.create(reOutputPath, showWarnings = FALSE)
 
       self$simulationMapping <- configurationPlan$SimulationMapping
@@ -57,10 +57,13 @@ ConfigurationPlan <- R6::R6Class(
   )
 )
 
-
+# Function taken from Matlab RE
+# Take Sections Tree input from .json, create folders
+# linearize Sections as a data.frame
 createRESections <- function(sectionsInput,
                              reInputPath,
                              reOutputPath) {
+  # Initialize Section Output
   sectionsOutput <- data.frame(
     id = NULL,
     title = NULL,
@@ -69,7 +72,8 @@ createRESections <- function(sectionsInput,
   )
 
   for (sectionIndex in seq(1, length(sectionsInput$Id))) {
-    # Create folder and add its path in the structure
+    # Create folder with numeric and name to ensure correct order
+    # TO DO: remove forbidden letters for folders
     path <- file.path(
       reOutputPath,
       sprintf(
@@ -81,18 +85,21 @@ createRESections <- function(sectionsInput,
 
     dir.create(path)
 
-    # Create title markdown within the folder
+    # Create title markdown "_title.md" within the folder
+    # This file has the actual section title
     knitr::knit(
       text = paste(sectionsInput$Title[sectionIndex], "\n"),
       output = file.path(path, "_title.md")
     )
 
-    # Copy Content
+    # Copy Content file indicated in sections with a consistent name "_content.md"
+    # for markdown joiner to read
     file.copy(
       file.path(reInputPath, sectionsInput$Content[sectionIndex]),
       file.path(path, "_content.md")
     )
 
+    # Fill the linearized Sections to be output
     sectionsOutput[
       length(sectionsOutput$id) + 1,
       c("id", "title", "content", "path")
