c3139283bcd0b4237ff6de9bf2f5c18ef5ee1079
Unknown <chelle.pierre@gmail.com>
2019-12-12 07:32:05 -0500

    Update test script for workflow

--- tests/dev/script-test.R
+++ tests/dev/script-test.R
@@ -6,9 +6,19 @@ library(tlf)
 library(ospsuite)
 library(ospsuite.reportingengine)
 
-sim <- loadSimulation("../tests/data/S1.pkml")
-pop <- loadPopulation("../tests/data/pop_10.csv")
+#sim <- loadSimulation("../tests/data/S1.pkml")
+#pop <- loadPopulation("../tests/data/pop_10.csv")
 
-popWorkflow <- PopulationWorkflow$new()
+popWorkflow <- PopulationWorkflow$new(outputFolder = "../tests/WorkflowOutput",
+                                      simulationPath = "../tests/data/S1.pkml",
+                                      populationPath = "../tests/data/pop_10.csv"
+                                      )
 
-plots <- popWorkflow$demographyPlot(sim)
+popWorkflow$runWorkflow()
+
+# This line is for showing the result as a subplots
+# It can be automatized later on when generating the report
+demographyPlots <- gridExtra::grid.arrange(grobs = popWorkflow$demographyPlot$output, layout_matrix = rbind(c(1,1),c(2,3)))
+plot(demographyPlots)
+
+plot(popWorkflow$timeProfilePlot$output)
