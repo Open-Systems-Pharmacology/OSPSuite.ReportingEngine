1d593fd2226743e5dbdae953b44b16fce03c73c1
Unknown <chelle.pierre@gmail.com>
2019-12-12 07:29:41 -0500

    Add print message to task
    Print message defines what is the task and if it is active and already done by the workflow

--- R/task.R
+++ R/task.R
@@ -7,7 +7,10 @@
 #' @field settings class
 #' @section Methods:
 #' \describe{
-#' \item{new()}{Initilialize Tasks settings}
+#' \item{new()}{Initilialize Task settings}
+#' \item{activate()}{Set Task as active in workflow}
+#' \item{inactivate()}{Set Task as inactive in workflow}
+#' \item{print()}{Show task settings}
 #' }
 #' @format NULL
 Task <- R6::R6Class(
@@ -17,16 +20,19 @@ Task <- R6::R6Class(
     input = NULL,
     output = NULL,
     settings = NULL,
+    message = NULL,
     
     initialize = function(input = NULL,
                           output = NULL, 
                           settings = NULL,
-                          active = TRUE){
+                          active = TRUE,
+                          message = NULL){
       validateIsOfType(active, "logical")
       self$active <- active
       self$input <- input
       self$output <- output
       self$settings <- settings
+      self$message <- message
       
     },
     activate = function(){
@@ -34,6 +40,13 @@ Task <- R6::R6Class(
     },
     inactivate = function(){
       self$active <- FALSE
+    },
+    
+    print = function(){
+      print(paste0("Task: ", self$message))
+      print(paste0("Task to be performed in workflow: ", self$active))
+      print(paste0("Task already performed in workflow: ", !is.null(self$output)))
+      invisible(self)
     }
   )
 )
