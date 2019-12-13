cb45b261add455f8240030008d6c20f2e399954f
Abdullah <aoh123@gmail.com>
2019-12-13 08:43:25 -0500

    Update test-massbalance.R

--- tests/dev/test-massbalance.R
+++ tests/dev/test-massbalance.R
@@ -5,32 +5,45 @@ library(ospsuite.reportingengine)
 
 wbsim <- loadSimulation("./data/individualPksimSim.pkml")
 
-
-
-
-#pth <- getAllPathStrings(wbsim,"**|Organism|**|smarties")
-
-
-getCompoundsList <- function(simulation){
-  moleculePaths <- getAllMoleculesMatching(paths = "**" , container = wbsim)
-  compoundsInPaths <- NULL
-  for (env in moleculePaths){
-    str <- toPathArray(env$path)
-    compoundsInPaths <- c(compoundsInPaths,tail(str, 1))
-  }
-  uniqueCompounds <- unique(compoundsInPaths)
-  return(uniqueCompounds)
-}
-
 compounds <- getCompoundsList(wbsim)
+pths <- getPathsForMoleculeAmount(simulation = wbsim,compounds[1])
 
 
-pth=list()
-for (cmp in compounds){
+print(wbsim$outputSelections)
+addOutputs(quantitiesOrPaths = pths[c(131,132)], simulation = wbsim)
+print(wbsim$outputSelections)
 
-  pth[[cmp]] <- getAllMoleculesMatching(paths = paste0("**|",cmp) , container = wbsim)
+res <- ospsuite::runSimulation(wbsim)
+df<-getOutputValues(simulationResults = res,quantitiesOrPaths = res$allQuantityPaths , individualIds = 0 )
 
-}
+
+# #Are molecule amounts the intersection of getAllContainersMatching and getALlMoleculesMatching ???
+# #NO - some parameters in containers, use getALlMoleculesMatching for mass balance, but you need to exclude Applications
+#
+# mn <- getAllMoleculesMatching(paths = "**|smarties" , container = wbsim)
+# cn <- getAllContainersMatching(paths = "**|smarties" , container = wbsim)
+#
+# cn_strs <- NULL
+# for (pp in cn){
+#   cn_strs <- c(cn_strs,pp$path)
+# }
+#
+#
+# mn_strs <- NULL
+# for (pp in mn){
+#   mn_strs <- c(mn_strs,pp$path)
+# }
+
+
+
+
+
+# pth=list()
+# for (cmp in compounds){
+#
+#   pth[[cmp]] <- getAllMoleculesMatching(paths = paste0("**|",cmp) , container = wbsim)
+#
+# }
 
 
 
