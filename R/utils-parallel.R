8fc8da049b5d4721886817d7f72909024fea7024
Abdullah <aoh123@gmail.com>
2020-01-09 13:34:50 -0500

    Create utils-parallel.R

---
+++ R/utils-parallel.R
@@ -0,0 +1,15 @@
+#' @title splitPopDataFile
+#' @description Function to split a population data file for parallel processing
+#' @description Input is a CSV population data file
+#' @description Output is vector of strings of the name of new CSV files
+#' @export
+splitPopDataFile <- function(fileName,folderName,numberOfSlaves,numberOfHeaderLines){
+  
+  return(newFileNames)
+}
+
+
+
+
+F<-read.csv(file = "C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine/data/popData.csv",skip = 2,check.names = FALSE)
+F
