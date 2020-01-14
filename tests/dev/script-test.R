09a4cfa2cca7ecc85df90cfa92dd6849dec33dda
Unknown <chelle.pierre@gmail.com>
2020-01-14 14:53:20 -0500

    update test

--- tests/dev/script-test.R
+++ tests/dev/script-test.R
@@ -1,19 +1,21 @@
 # Test Script for workflow
 
-rm(list=ls())
+rm(list = ls())
 
 library(tlf)
 library(ospsuite)
 library(ospsuite.reportingengine)
 
-#sim <- loadSimulation("../tests/data/S1.pkml")
-#pop <- loadPopulation("../tests/data/pop_10.csv")
+# sim <- loadSimulation("../tests/data/S1.pkml")
+# pop <- loadPopulation("../tests/data/pop_10.csv")
 
 # This define the workflow structure
-popWorkflow <- PopulationWorkflow$new(outputFolder = "../tests/WorkflowOutput",
-                                      simulationPath = "../tests/data/S1.pkml",
-                                      populationPath = "../tests/data/pop_10.csv"
-                                      )
+popWorkflow <- PopulationWorkflow$new(
+  simulationFile = "../tests/data/S1.pkml",
+  populationFile = "../tests/data/pop_10.csv"
+)
+output1 <- "../S1_pop_10/"
+output2 <- "../S1_pop_20/"
 
 # The print method gives an overview of the task list and if they are active
 popWorkflow
@@ -30,7 +32,7 @@ popWorkflow$runWorkflow()
 
 # This line is for showing the result as a subplots
 # It can be automatized later on when generating the report
-#demographyPlots <- gridExtra::grid.arrange(grobs = popWorkflow$demographyPlot$output, layout_matrix = rbind(c(1,1),c(2,3)))
-#plot(demographyPlots)
+# demographyPlots <- gridExtra::grid.arrange(grobs = popWorkflow$demographyPlot$output, layout_matrix = rbind(c(1,1),c(2,3)))
+# plot(demographyPlots)
 
-#plot(popWorkflow$gofPlot$output[["timeProfile"]])
+# plot(popWorkflow$gofPlot$output[["timeProfile"]])
