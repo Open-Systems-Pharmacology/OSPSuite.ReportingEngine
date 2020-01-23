e7129df426b40dc8f7ad8e287b5dccf3caa526e6
Abdullah <aoh123@gmail.com>
2020-01-23 01:51:40 -0500

    update sensitivity analysis parallel mode

--- tests/dev/test-sensitivity-par.R
+++ tests/dev/test-sensitivity-par.R
@@ -4,40 +4,7 @@ library(ospsuite.reportingengine)
 library(Rmpi)
 
 
-performSensitivityAnalysis <- function(simFileName,
-                                       simFileFolder,
-                                       paramRange=NULL,
-                                       pkParameterName = "AUC",
-                                       totalSensitivityThreshold = 1){
-  sim<-loadSimulation(paste0(simFileFolder,simFileName))
-  allParameters <- ospsuite::getAllParametersMatching(paths = "**" , container = sim)
-  outputSelections <- sim$outputSelections
 
-  if (is.null(paramRange)){
-    parameters <- allParameters
-  }
-  else{
-    parameters <- allParameters[ paramRange ]
-  }
-
-  sensitivityAnalysis <- SensitivityAnalysis$new(simulation = sim,
-                                                 parameters = parameters )
-
-  sensitivityAnalysisRunOptions <- SensitivityAnalysisRunOptions$new(showProgress = FALSE)
-
-  results <- runSensitivityAnalysis(sensitivityAnalysis = sensitivityAnalysis,
-                                    sensitivityAnalysisRunOptions = sensitivityAnalysisRunOptions)
-
-  pkSensitivities <- list()
-  for (output in outputSelections$allOutputs) {
-    pkSensitivities <- results$allPKParameterSensitivitiesFor(
-      pkParameterName = "AUC",
-      outputPath = output$path,
-      totalSensitivityThreshold = totalSensitivityThreshold
-    )
-  }
-  print(pkSensitivities)
-}
 
 
 #simFileName <- "individualPksimSim.pkml"
@@ -47,8 +14,10 @@ simFileFolder <- "C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine/
 sim<-loadSimulation(paste0(simFileFolder,simFileName))
 #sim<-loadSimulation("./data/simpleMobiEventSim.pkml")
 
+print("Getting param list...")
 allParameters <- ospsuite::getAllParametersMatching(paths = "**" , container = sim)
 totalNumberParameters <- length(allParameters)
+print("...done")
 
 numberOfCores <- 1
 numParametersPerCore <- ceiling(totalNumberParameters/numberOfCores)
@@ -65,12 +34,12 @@ if (numberOfCores>1){
   mpi.bcast.Robj2slave(obj = simFileName)
   mpi.bcast.Robj2slave(obj = simFileFolder)
   mpi.bcast.Robj2slave(obj = paramIndices)
-  mpi.bcast.cmd(performSensitivityAnalysis(simFileName,simFileFolder,paramIndices[[mpi.comm.rank()]]))
+  mpi.bcast.cmd(ospsuite.reportingengine::performSensitivityAnalysis(simFileName,simFileFolder,paramIndices[[mpi.comm.rank()]]))
   mpi.close.Rslaves()
 } else
 {
-  paramIndices[[1]] <- c(1,totalNumberParameters)
-  resa <- performSensitivityAnalysis(simFileName,simFileFolder)
+  #paramIndices[[1]] <- c(1,totalNumberParameters)
+  resa <- ospsuite.reportingengine::performSensitivityAnalysis(simFileName,simFileFolder)
 }
 
 
