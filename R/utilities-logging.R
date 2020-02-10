0edcc04cc6240b5b288b2806dc1552716d8a0f13
Unknown <chelle.pierre@gmail.com>
2020-02-10 03:31:12 -0500

    Fixes #50 draft utilities-logging methods

---
+++ R/utilities-logging.R
@@ -0,0 +1,51 @@
+#' @title logError
+#' @description Save error messages into a log error file
+#' @param message message to save in log file
+#' @param file log file name.
+#' Default is `log-error.txt`
+#' @param printConsole logical to print error on console
+#' @return 
+#' @export
+logError <- function(message, 
+                     file = "log-error.txt", 
+                     printConsole = TRUE){
+
+  if (printConsole) {
+    print(message)
+  }
+  return(message)
+}
+
+#' @title logDebug
+#' @description Save intermediate messages into a log debug file
+#' @param message message to save in log file
+#' @param file log file name.
+#' Default is `log-debug.txt`
+#' @param printConsole logical to print error on console
+#' @return 
+#' @export
+logDebug <- function(message, 
+                     file = "log-debug.txt", 
+                     printConsole = TRUE){
+  if (printConsole) {
+    print(message)
+  }
+  return(message)
+}
+
+#' @title logInfo
+#' @description Save info messages into a log info file
+#' @param message message to save in log file
+#' @param file log file name.
+#' Default is `log-info.txt`
+#' @param printConsole logical to print error on console
+#' @return 
+#' @export
+loglogInfo <- function(message, 
+                     file = "log-info.txt", 
+                     printConsole = TRUE){
+  if (printConsole) {
+    print(message)
+  }
+  return(message)
+} 
