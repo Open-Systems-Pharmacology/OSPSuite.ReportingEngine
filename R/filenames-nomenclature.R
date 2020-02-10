fdf2aea778b5441ba0fb7a0a7b4ac809989608a1
Unknown <chelle.pierre@gmail.com>
2020-02-10 04:45:16 -0500

    Fixes #52 default folder names

--- R/filenames-nomenclature.R
+++ R/filenames-nomenclature.R
@@ -10,10 +10,19 @@ defaultFileNames <- list(
   },
   pkAnalysisFile = function(simulationName){
     getDefaultFileName(simulationName, suffix = "PK-Analyses")
+  },
+  reportFolder = function(name = "Report"){
+    getDefaultFolderName(name)
+  },
+  resultsFolder = function(name = "Results"){
+    getDefaultFolderName(name)
+  },
+  figuresFolder = function(name = "Figures"){
+    getDefaultFolderName(name)
   }
 )
 
-#' @title getDefaultFileNames
+#' @title getDefaultFileName
 #' @description Add suffix and extension to create a default file name
 #' for population files, results files and PK-analysis files.
 #' @param ... names 
@@ -25,4 +34,16 @@ getDefaultFileName <- function(..., suffix, extension = "csv", sep = "-"){
   defaultName <- paste(..., suffix, sep = sep)
   defaultFileName <- paste0(defaultName, ".", extension)
   return(defaultFileName)
+}
+
+#' @title getDefaultFolderName
+#' @description Create a default folder name for reporting engine results and figures
+#' @param ... names 
+#' @param suffix Suffix to be added at the end of the folder name.
+#' Dafault `suffix` is the curent date
+#' @param sep separation between names and suffix. Default is `-`
+#' @return default filename adding default suffix and fileextension
+getDefaultFolderName <- function(..., suffix = format(Sys.Date(), "%Y%m%d"), sep = "-"){
+  defaultFolderName <- paste(..., suffix, sep = sep)
+  return(defaultFolderName)
 }
