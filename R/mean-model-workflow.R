485bdff98a011a95465d9b2c8e249870de7f07bf
Unknown <chelle.pierre@gmail.com>
2020-02-11 07:02:32 -0500

    Mean model workflow redesign and documentation

--- R/mean-model-workflow.R
+++ R/mean-model-workflow.R
@@ -1,15 +1,13 @@
 #' @title MeanModelWorkflow
-#' @docType class
-#' @description  Mean Model Workflow Task for Reporting Engine
-#' @field meanModelSimulation Population simulation task
-#' @field pkParametersCalculation PK parameters calculation task
-#' @field sensitivityAnalysis Sensitivity analysis task
-#' @section Methods:
-#' \describe{
-#' \item{new()}{Initilialize reporting engine mean model workflow}
-#' \item{setMeanModelSimulationSettings()}{Define mean model simulation task settings}
-#' \item{runWorkflow()}{Run the active tasks of mean model worklfow}
-#' }
+#' @description  R6 class for Reporting Engine Mean Model Workflow
+#' @field meanModelSimulation R6 class `Task` for simulation
+#' @field meanModelPKParameters R6 class `Task` for PK parameters calculation
+#' @field meanModelSensitivityAnalysis R6 class `Task` for sensitivity analysis
+#' @field plotGoF R6 class `Task` for goodness of fit plots
+#' @field plotMassBalance R6 class `Task` for mass balance plot
+#' @field plotAbsorption R6 class `Task` for absorption plot
+#' @field plotPKParameters R6 class `Task` for PK parameters plot
+#' @field plotSensitivity R6 class `Task` for sensitivity plot
 #' @export
 #' @import tlf
 #' @import ospsuite
@@ -19,29 +17,42 @@ MeanModelWorkflow <- R6::R6Class(
   inherit = Workflow,
 
   public = list(
-    meanModelSimulation = NULL,
-    meanModelPKParameters = NULL,
-    meanModelSensitivityAnalysis = NULL,
+    meanModelSimulation = NULL, # TO DO: rename with simpler task name simulate
+    meanModelPKParameters = NULL, # TO DO: rename with simpler task name calculatePKParameters
+    plotGoF = NULL,
+    plotMassBalance = NULL,
+    plotAbsorption = NULL,
+    plotPKParameters = NULL,
+    meanModelSensitivityAnalysis = NULL, # TO DO: rename with simpler task name calculateSensitivity
+    plotSensitivity = NULL,
 
+    #' @description
+    #' Create a new `MeanModelWorkflow` object.
+    #' @param ... input parameters inherited from R6 class object `Workflow`.
+    #' @return A new `MeanModelWorkflow` object
     initialize = function(...) {
       super$initialize(...)
 
-
-      # self$setDemographyPlotSettings()
-      # self$setGofPlotSettings()
-      # self$setpkParametersPlotSettings()
-      # self$setSensitivityPlotSettings()
+      # TO DO: include task parameters from initialization ?
+      meanModelSimulationSettings()
+      self$calculatePKParametersSettings()
+      self$plotGoFSettings()
+      self$plotMassBalanceSettings()
+      self$plotAbsorptionSettings()
+      self$plotPKParametersSettings()
+      self$meanModelSensitivityAnalysisSettings()
+      self$plotSensitivitySettings()
     },
 
-    setMeanModelSimulationSettings = function(input = NULL,
-                                              output = NULL,
-                                              settings = NULL,
-                                              active = TRUE,
-                                              message = NULL,
-                                              inputFolderName = self$inputFolder,
-                                              simulationFileName = self$simulation,
-                                              resultsFolderName = self$simulationFolder,
-                                              resultsFileName = "meanModelSimulation") {
+    meanModelSimulationSettings = function(input = NULL,
+                                               output = NULL,
+                                               settings = NULL,
+                                               active = TRUE,
+                                               message = NULL,
+                                               inputFolderName = self$inputFolder,
+                                               simulationFileName = self$simulation,
+                                               resultsFolderName = self$simulationFolder,
+                                               resultsFileName = "meanModelSimulation") {
       self$meanModelSimulation <- SimulationTask$new(
         input = input,
         output = output,
@@ -55,16 +66,16 @@ MeanModelWorkflow <- R6::R6Class(
       )
     },
 
-    setMeanModelPKParameterSettings = function(input = NULL,
-                                              output = NULL,
-                                              settings = NULL,
-                                              active = TRUE,
-                                              message = NULL,
-                                              simulationFilePath = file.path(self$inputFolder,paste0(self$simulation,".pkml")),
-                                              simulationResultFilePaths = self$meanModelSimulation$generatedResultFileNames,
-                                              pkParametersToEvaluate = NULL,
-                                              userDefinedPKFunctions = NULL,
-                                              pkParameterResultsFilePath = file.path(self$pkParametersFolder,"meanModelPKParameters.csv")){
+    calculatePKParametersSettings = function(input = NULL,
+                                                 output = NULL,
+                                                 settings = NULL,
+                                                 active = TRUE,
+                                                 message = NULL,
+                                                 simulationFilePath = file.path(self$inputFolder, paste0(self$simulation, ".pkml")),
+                                                 simulationResultFilePaths = self$meanModelSimulation$generatedResultFileNames,
+                                                 pkParametersToEvaluate = NULL,
+                                                 userDefinedPKFunctions = NULL,
+                                                 pkParameterResultsFilePath = file.path(self$pkParametersFolder, "meanModelPKParameters.csv")) {
       self$meanModelPKParameters <- CalculatePKParametersTask$new(
         input = input,
         output = output,
@@ -75,19 +86,20 @@ MeanModelWorkflow <- R6::R6Class(
         simulationResultFilePaths = simulationResultFilePaths,
         pkParametersToEvaluate = pkParametersToEvaluate,
         userDefinedPKFunctions = userDefinedPKFunctions,
-        pkParameterResultsFilePath = pkParameterResultsFilePath)
+        pkParameterResultsFilePath = pkParameterResultsFilePath
+      )
     },
 
-    setMeanModelSensitivityAnalysisSettings = function(input = NULL,
-                                                       output = NULL,
-                                                       settings = NULL,
-                                                       active = TRUE,
-                                                       message = NULL,
-                                                       inputFolderName = self$inputFolder,
-                                                       simulationFileName = self$simulation,
-                                                       resultsFolderName = self$sensitivityFolder,
-                                                       resultsFileName = "meanModelSensitivityAnalysis",
-                                                       numberOfCores = 1) {
+    meanModelSensitivityAnalysisSettings = function(input = NULL,
+                                                        output = NULL,
+                                                        settings = NULL,
+                                                        active = TRUE,
+                                                        message = NULL,
+                                                        inputFolderName = self$inputFolder,
+                                                        simulationFileName = self$simulation,
+                                                        resultsFolderName = self$sensitivityFolder,
+                                                        resultsFileName = "meanModelSensitivityAnalysis",
+                                                        numberOfCores = 1) {
       self$meanModelSensitivityAnalysis <- SensitivityAnalysisTask$new(
         input = input,
         output = output,
@@ -102,27 +114,69 @@ MeanModelWorkflow <- R6::R6Class(
       )
     },
 
+    # TO DO: Define the tasks settings for plots
+    plotGoFSettings = function(input = NULL,
+                                   output = NULL,
+                                   active = TRUE,
+                                   message = NULL) {
+      self$plotGoF <- Task$new(
+        input = input,
+        output = output,
+        active = active,
+        message = "Plot Goodness of Fit task not available at the moment"
+      )
+    },
 
-    # setGofPlotSettings = function(input = NULL,
-    #                                   output = NULL,
-    #                                   active = TRUE,
-    #                                   message = NULL) {
-    #   self$gofPlot <- Task$new(
-    #     input = input %||% list(
-    #       "population" = file.path(self$inputFolder, paste0(self$population, ".csv")),
-    #       "populationSimulation" = self$populationSimulation$output$populationSimulation
-    #     ),
-    #     output = output %||% list(
-    #       "gofResults" = file.path(self$simulationFolder, "gofResults.RData"),
-    #       "gofPlot" = file.path(self$pkParametersFolder, "gofPlot")
-    #     ),
-    #     active = active,
-    #     message = message %||% "Plot goodness of fit diagnostics"
-    #   )
-    # },
+    plotPKParametersSettings = function(input = NULL,
+                                            output = NULL,
+                                            active = TRUE,
+                                            message = NULL) {
+      self$plotPKParameters <- Task$new(
+        input = input,
+        output = output,
+        active = active,
+        message = "Plot PK parameters task not available at the moment"
+      )
+    },
+
+    plotMassBalanceSettings = function(input = NULL,
+                                           output = NULL,
+                                           active = TRUE,
+                                           message = NULL) {
+      self$plotMassBalance <- Task$new(
+        input = input,
+        output = output,
+        active = active,
+        message = "Plot Mass Balance task not available at the moment"
+      )
+    },
+
+    plotAbsorptionSettings = function(input = NULL,
+                                          output = NULL,
+                                          active = TRUE,
+                                          message = NULL) {
+      self$plotAbsorption <- Task$new(
+        input = input,
+        output = output,
+        active = active,
+        message = "Plot Absorption task not available at the moment"
+      )
+    },
+
+    plotSensitivitySettings = function(input = NULL,
+                                           output = NULL,
+                                           active = TRUE,
+                                           message = NULL) {
+      self$plotPKParameters <- Task$new(
+        input = input,
+        output = output,
+        active = active,
+        message = "Plot Sensitivity task not available at the moment"
+      )
+    },
 
     runWorkflow = function() {
-
+      logInfo(message = "Start of mean model workflow")
 
       # MEAN MODEL WORKFLOW
       # CORE STAGE 1:  SIMULATION
@@ -136,38 +190,32 @@ MeanModelWorkflow <- R6::R6Class(
       # CORE STAGE 3:  CALCULATE SENSITIVITY
       # 3a - Plots and Tables based on sensitivity results
 
-
-
-
-      print("Start of mean model workflow: ")
-      print(self$reportingEngineInfo)
-
-
-
       if (self$meanModelSimulation$active) {
+        # TO DO: include simulationSets
+        # for (simulationSet in self$simulationSets){}
         if (self$meanModelSimulation$validateInput()) {
-          print("Starting mean model simulation")
-            self$meanModelSimulation$generatedResultFileNames <-simulateModel(
+          logInfo(message = "Starting mean model simulation")
+          self$meanModelSimulation$generatedResultFileNames <- meanModelSimulationModel(
             simFilePath = file.path(self$meanModelSimulation$inputFolderName, paste0(self$meanModelSimulation$simulationFileName, ".pkml")),
-            resultsFilePath = file.path(self$meanModelSimulation$resultsFolderName, paste0(self$meanModelSimulation$resultsFileName, ".csv")))
+            resultsFilePath = file.path(self$meanModelSimulation$resultsFolderName, paste0(self$meanModelSimulation$resultsFileName, ".csv"))
+          )
         }
       }
-
 
       if (self$meanModelPKParameters$active) {
         if (self$meanModelPKParameters$validateInput()) {
-          print("Starting mean model PK parameters calculation")
+          logInfo(message = "Starting mean model PK parameters calculation")
           self$meanModelPKParameters$generatedResultFileNames <- calculatePKParameters(
             simulationFilePath = self$meanModelPKParameters$simulationFilePath,
             simulationResultFilePaths = self$meanModelSimulation$generatedResultFileNames,
-            pkParameterResultsFilePath = self$meanModelPKParameters$pkParameterResultsFilePath)
+            pkParameterResultsFilePath = self$meanModelPKParameters$pkParameterResultsFilePath
+          )
         }
       }
 
-
       if (self$meanModelSensitivityAnalysis$active) {
         if (self$meanModelSensitivityAnalysis$validateInput()) {
-          print("Starting mean model sensitivity analysis")
+          logInfo(message = "Starting mean model sensitivity analysis")
           self$meanModelSensitivityAnalysis$generatedResultFileNames <- analyzeSensitivity(
             simFilePath = file.path(self$meanModelSensitivityAnalysis$inputFolderName, paste0(self$meanModelSensitivityAnalysis$simulationFileName, ".pkml")),
             resultsFileFolder = file.path(self$meanModelSensitivityAnalysis$resultsFolderName),
@@ -176,92 +224,39 @@ MeanModelWorkflow <- R6::R6Class(
           )
         }
       }
+
+      # TO DO: plug plot tasks to actual results
+      if (self$plotGoF$active) {
+        logInfo(message = self$plotGoF$message)
+      }
+      if (self$plotMassBalance$active) {
+        logInfo(message = self$plotMassBalance$message)
+      }
+      if (self$plotAbsorption$active) {
+        logInfo(message = self$plotAbsorption$message)
+      }
+      if (self$plotPKParameters$active) {
+        logInfo(message = self$plotPKParameters$message)
+      }
+      if (self$plotSensitivity$active) {
+        logInfo(message = self$plotSensitivity$message)
+      }
     },
 
-
-
-    # if (self$pkParametersCalculation$active) {
-    #   # if (self$pkParametersCalculation$validateInput()){
-    #   # calculatePKParameters()
-    #   # }
-    # }
-    # if (self$sensitivityAnalysis$active) {
-    #   if (self$sensitivityAnalysis$validateInput()) {
-    #     simulation <- loadSimulation(self$demographyPlot$input$simulation)
-    #
-    #     pkSensitivities <- analyzeSensitivity(simulation = simulation)
-    #     save(pkSensitivities, file = self$sensitivityAnalysis$output$sensitivityAnalysis)
-    #   }
-    # }
-    # if (self$demographyPlot$active) {
-    #   if (self$demographyPlot$validateInput()) {
-    #     population <- loadPopulation(self$demographyPlot$input$population)
-    #     simulation <- loadSimulation(self$demographyPlot$input$simulation)
-    #
-    #     # The last properties of plotDemograpy will be set within task settings
-    #     demographyPlot <- plotDemography(
-    #       simulation = simulation,
-    #       population = population,
-    #       parameterNames = c(StandardPath$Age, StandardPath$Weight, StandardPath$Height),
-    #       plotConfiguration = NULL
-    #     )
-    #
-    #     save(demographyPlot, file = self$demographyPlot$output$demographyResults)
-    #     dir.create(self$demographyPlot$output$demographyPlot)
-    #     for (plotName in names(demographyPlot)) {
-    #       ggplot2::ggsave(
-    #         filename = file.path(self$demographyPlot$output$demographyPlot, paste0(removeForbiddenLetters(plotName), ".png")),
-    #         plot = demographyPlot[[plotName]]
-    #       )
-    #     }
-    #   }
-    # }
-    # if (self$gofPlot$active) {
-    #   if (self$gofPlot$validateInput()) {
-    #     load(file = self$gofPlot$input$populationSimulation)
-    #     population <- loadPopulation(self$gofPlot$input$population)
-    #     observedData <- self$gofPlot$input$observedData
-    #
-    #     gofPlot <- plotGoodnessOfFit(
-    #       populationSimulation = populationSimulation,
-    #       population = population,
-    #       observedData = observedData,
-    #       quantity = NULL,
-    #       plotConfiguration = NULL
-    #     )
-    #
-    #     save(gofPlot, file = self$gofPlot$output$gofResults)
-    #     dir.create(self$gofPlot$output$gofPlot)
-    #     for (plotName in names(gofPlot)) {
-    #       ggplot2::ggsave(
-    #         filename = file.path(self$gofPlot$output$gofPlot, paste0(removeForbiddenLetters(plotName), ".png")),
-    #         plot = gofPlot[[plotName]]
-    #       )
-    #     }
-    #   }
-    # }
-    # if (self$pkParametersPlot$active) {
-    #   self$pkParametersPlot$output <- plotPKParameters()
-    # }
-    # if (self$sensitivityPlot$active) {
-    #   if (self$sensitivityPlot$validateInput()) {
-    #     load(file = self$sensitivityPlot$input$sensitivityAnalysis)
-    #
-    #     sensitivityPlot <- plotSensitivity(sensitivityAnalysis)
-    #     save(sensitivityPlot, file = file.path(self$sensitivityPlot$output$sensitivityPlot))
-    #   }
-    # }
-
-
+    #' @description
+    #' Print workflow list of tasks
+    #' TO DO: add simulationSets to print() method
+    #' @return Task list information
     print = function() {
       taskOrder <- list(
-        "Task 1" = self$populationSimulation$print(),
-        "Task 2" = self$pkParametersCalculation$print(),
-        "Task 3" = self$sensitivityAnalysis$print(),
-        "Task 4" = self$demographyPlot$print(),
-        "Task 5" = self$gofPlot$print(),
-        "Task 6" = self$pkParametersPlot$print(),
-        "Task 7" = self$sensitivityPlot$print()
+        "Task 1" = self$meanModelSimulation$print(),
+        "Task 2" = self$meanModelPKParameters$print(),
+        "Task 3" = self$meanModelSensitivityAnalysis$print(),
+        "Task 4" = self$plotGoF$print(),
+        "Task 5" = self$plotMassBalance$print(),
+        "Task 6" = self$plotAbsorption$print(),
+        "Task 7" = self$plotPKParameters$print(),
+        "Task 8" = self$plotSensitivity$print()
       )
       invisible(self)
 
