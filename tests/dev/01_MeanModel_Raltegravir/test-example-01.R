b995e94529852aa9cc9ff6cdab5062a7cc3b7557
Unknown <chelle.pierre@gmail.com>
2020-03-06 06:15:27 -0500

    Update test example

--- tests/dev/01_MeanModel_Raltegravir/test-example-01.R
+++ tests/dev/01_MeanModel_Raltegravir/test-example-01.R
@@ -3,23 +3,29 @@ library(ospsuite)
 library(tlf)
 library(ospsuite.reportingengine)
 
-setwd("C:/Design2Code/R-RE-tests/01_MeanModel_Raltegravir")
+setwd("./tests/dev/01_MeanModel_Raltegravir")
 
 simFiles <- paste0("PKML/", list.files("PKML"))
 Raltegravir_PKFile <- "Raltegravir_PK.csv"
 tpDictionaryFile <- "tpDictionary.csv"
 
+dataFilters <- list("10mg_", "25mg", "50mg", "100mg", "200mg", "400mg", "800mg", "1200mg", "1600mg", 
+                 "100mg_FCT_MD", "200mg_FCT_MD", "400mg_FCT", "400mg_FCT_MD", "Chewable_tablet_fasted", "Chewable_tablet_fed", "granules_suspension")
+names(dataFilters) <- simFiles
+
+# Shorten the process by limiting to only 2 simulation sets
 simFiles <- simFiles[1:2]
+
 # Initilize list of mean model sets
 meanModelSets <- list()
 for (simFile in simFiles){
 modelSet <-  MeanModelSet$new(simulationFile = simFile,
                                   pathID = 'Organism|PeripheralVenousBlood|Raltegravir|Plasma (Peripheral Venous Blood)',
                                   pathName = 'Raltegravir',
-                                  pathUnit = 'nmol/l',
+                                  pathUnit = 'µg/l',
                                   pkParameters = c('C_max','C_max_norm','t_max','C_tEnd','AUC','AUC_norm','AUC_inf','AUC_inf_norm','MRT','Thalf','CL','Vss','Vd'),
                                   pkParametersUnits = c('μg/l','kg/l','h','μg/l','μg*h/l','kg*h/l','μg*h/l','kg*h/l','h','h','ml/min/kg','ml/kg','ml/kg'),
-                                  dataFilter = list(variableNames = "Grouping", values = "values"),
+                                  dataFilter = dataFilters[simFile],
                                   observedDataFile = Raltegravir_PKFile,
                                   observedMetaDataFile = tpDictionaryFile)
 
@@ -29,14 +35,9 @@ meanModelSets[[modelSet$simulationSetName]] <- modelSet
 # Initialize workflow structure
 meanModel_Raltegravir <- MeanModelWorkflow$new(simulationSets = meanModelSets)
 
-# Tasks to activate/inactivate
+# Tasks to activate/inactivate (this can also be done using meanModel_Raltegravir$activateTasks() method)
 activateWorkflowTasks(meanModel_Raltegravir, tasks = c("plotGoF", "plotAbsorption", "plotMassBalance"))
 inactivateWorkflowTasks(meanModel_Raltegravir, tasks = c("meanModelPKParameters"))
 
-# Define options specific to the tasks: e.g. plot configuration,
-# selected compounds here
-meanModel_Raltegravir$plotMassBalance$settings$compoundNames <- c("Raltegravir", "Raltegravir-UGT1A1-Kassahun 2007 Metabolite", "Raltegravir-UGT1A9-Kassahun 2007 Metabolite")
-
 # Run the mean model workflow
 meanModel_Raltegravir$runWorkflow()
-
