905dac3898249f345571e21c26313f510a640a72
Unknown <chelle.pierre@gmail.com>
2020-03-30 12:32:11 -0400

    Script example for reading/using observed data and filters

--- tests/dev/script-read-observed-data.R
+++ tests/dev/script-read-observed-data.R
@@ -1,6 +1,5 @@
 library(ospsuite.reportingengine)
 
-
 # Read Observed Data from Example 01 - MeanModel Raltegravir
 observedData <- readObservedDataFile("01_MeanModel_Raltegravir/Raltegravir_PK.csv")
 head(observedData)
@@ -8,3 +7,14 @@ head(observedData)
 # Read Dictionary from Example 01 - MeanModel Raltegravir
 dictionary <- readObservedDataFile("01_MeanModel_Raltegravir/tpDictionary.csv")
 dictionary
+
+# data filters
+dataFilters <- c('Grouping %in% "10mg_"', 
+                 'Grouping %in% "50mg"')
+
+# Function called internally by wokflow
+rowFilter10mg <- evalDataFilter(observedData, parse(text = dataFilters[1]))
+#rowFilter50mg <- evalDataFilter(observedData, parse(text = dataFilters[2]))
+
+observedData[rowFilter10mg,]
+#observedData[rowFilter50mg,]
