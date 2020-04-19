6623265ab97c44ec33d395709e5b95b7cd8e1995
Abdullah <aoh123@gmail.com>
2020-04-19 19:50:50 -0400

    add path id to mean example

--- tests/dev/test-mean.R
+++ tests/dev/test-mean.R
@@ -4,11 +4,13 @@ devtools::load_all(".")
 tree <- getSimulationTree("./tests/dev/individualPksimSim.pkml")
   ms <- SimulationSet$new(
   simulationSetName = "meansim",
-  simulationFile = "./tests/dev/individualPksimSim.pkml"
+  simulationFile = "./tests/dev/individualPksimSim.pkml",
+  pathID = tree$Organism$Heart$Interstitial$smarties$Concentration$path
 )
+
 mwf <- MeanModelWorkflow$new(simulationSets = list(ms))
 mwf$simulate$settings$showProgress <- TRUE
-mwf$meanModelPKParameters$activate()
-mwf$meanModelSensitivityAnalysis$activate()
-mwf$meanModelSensitivityAnalysis$settings$variableParameterPaths <- c(tree$Organism$Heart$Volume$path)
+mwf$meanModelPKParameters$inactivate()
+mwf$meanModelSensitivityAnalysis$inactivate()
+#mwf$meanModelSensitivityAnalysis$settings$variableParameterPaths <- c(tree$Organism$Heart$Volume$path)
 mwf$runWorkflow()
