4f9a9407ef136571c05c6ee7ba666584d9fbb936
Abdullah <aoh123@gmail.com>
2020-04-19 20:50:32 -0400

    Revert "add path id to pop example"
    This reverts commit aa76c5c9195021ccbbffa4d71cb584a094de44c9.

--- tests/dev/test-nonparallel-population.R
+++ tests/dev/test-nonparallel-population.R
@@ -2,18 +2,15 @@ rm(list = ls())
 library(ospsuite)
 devtools::load_all(".")
 tree <- getSimulationTree("./tests/dev/individualPksimSim.pkml")
-ps <- PopulationSimulationSet$new(simulationSetName = "nonparpopsim",
+ps <- PopulationSimulationSet$new(
+  simulationSetName = "nonparpopsim",
   simulationFile = "./tests/dev/individualPksimSim.pkml",
-  populationFile = "./tests/dev/popData_short.csv",
-  pathID = tree$Organism$VenousBlood$Plasma$smarties$Concentration$path
+  populationFile = "./tests/dev/popData_short.csv"
 )
-pwf <- PopulationWorkflow$new(simulationSets = ps)
+pwf <- PopulationWorkflow$new(simulationSets = list(ps))
 pwf$simulatePopulation$settings$showProgress <- TRUE
-pwf$simulatePopulation$activate()
-pwf$populationPKParameters$inactivate()
-pwf$populationSensitivityAnalysis$inactivate()
 
-#pwf$populationSensitivityAnalysis$settings$variableParameterPaths <-  tree$Organism$Heart$Volume$path
-#pwf$populationSensitivityAnalysis$settings$pkParameterSelection <- c("C_max")
-#pwf$populationSensitivityAnalysis$settings$quantileVec <- c(0.5)
+pwf$populationSensitivityAnalysis$settings$variableParameterPaths <-  tree$Organism$Heart$Volume$path
+pwf$populationSensitivityAnalysis$settings$pkParameterSelection <- c("C_max")
+pwf$populationSensitivityAnalysis$settings$quantileVec <- c(0.5)
 pwf$runWorkflow()
