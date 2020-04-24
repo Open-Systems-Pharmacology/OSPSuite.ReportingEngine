8f97a4642f4934e940220e55c6c986903b451b48
Unknown <chelle.pierre@gmail.com>
2020-04-24 13:15:13 -0400

    Introduce plotPopulationPKparameters
    WIP: 
    @Yuri05 I wanted to include ratio and x, but was not sure of what the features are exactly. Between which population or parameters do we need to calculate the ratio ? Same for x, is a VPC the plot output when x is input ?

--- R/utilities-calculate-pk-parameters.R
+++ R/utilities-calculate-pk-parameters.R
@@ -55,3 +55,78 @@ plotMeanPKParameters <- function(structureSet,
     tables = list(pkAnalysis = pkParametersTable)
   ))
 }
+
+
+#' @title plotPopulationPKParameters
+#' @description Plot PK parameters box plots and tables
+#' @param structureSets `SimulationStructure` R6 class object
+#' @param logFolder folder where the logs are saved
+#' @param settings list of settings for the output table/plot
+#' @param workflowType workflowType Type of population workflow.
+#' Use enum `PopulationWorkflowTypes` to get list of workflow types.
+#' @return data.frame with calculated PK parameters for the simulation set
+#' @export
+#' @import ospsuite
+plotPopulationPKParameters <- function(structureSets,
+                                       logFolder = getwd(),
+                                       settings = NULL,
+                                       workflowType = PopulationWorkflowTypes$parallelComparison) {
+  validateIsIncluded(workflowType, PopulationWorkflowTypes)
+
+  pkParametersTableAcrossPopulations <- NULL
+  pkRatioTableAcrossPopulations <- NULL
+  
+  for (index in seq_along(popW$simulationStructures))
+  {
+    structureSet <- popW$simulationStructures[[index]]
+
+    simulation <- loadSimulationWithUpdatedPaths(structureSet$simulationSet)
+    pkAnalyses <- ospsuite::importPKAnalysesFromCSV(
+      structureSet$pkAnalysisResultsFileNames,
+      simulation
+    )
+
+    pkParametersTable <- ospsuite::pkAnalysesAsDataFrame(pkAnalyses)
+
+    pkParametersTable$Population <- structureSet$simulationSet$populationName
+
+    pkParametersTableAcrossPopulations <- rbind.data.frame(
+      pkParametersTableAcrossPopulations,
+      pkParametersTable
+    )
+  }
+
+  pkParametersPlots <- NULL
+  pkParametersTables <- NULL
+  
+  pkParametersMapping <- tlf::BoxWhiskerDataMapping$new(
+    x = "Population",
+    y = "Value"
+  )
+
+  for (parameter in levels(pkParametersTableAcrossPopulations$Parameter)) {
+    pkParameterData <- pkParametersTableAcrossPopulations[pkParametersTableAcrossPopulations$Parameter %in% parameter, ]
+    pkParameterData <- pkParameterData[!is.na(pkParameterData$Value), ]
+    pkParameterMetaData <- list("Value" = list(
+      dimension = parameter,
+      unit = pkParameterData$Unit[1]
+    ))
+    pkParametersPlots[[parameter]] <- tlf::plotBoxWhisker(
+      data = pkParameterData,
+      metaData = pkParameterMetaData,
+      dataMapping = pkParametersMapping,
+      plotConfiguration = settings$plotConfiguration
+    ) +
+      ggplot2::labs(title = NULL, subtitle = NULL)
+
+    pkParametersTables[[parameter]] <- tlf::getBoxWhiskerMeasure(
+      data = pkParameterData,
+      dataMapping = pkParametersMapping
+    )
+  }
+  
+  return(list(
+    plots = pkParametersPlots,
+    tables = pkParametersTables
+  ))
+}
