be2b2f38fa525369d1397496a64e5da3360c4aa0
Abdullah <aoh123@gmail.com>
2020-04-28 07:25:40 -0400

    remove bars in file name

--- R/utilities-sensitivity-analysis.R
+++ R/utilities-sensitivity-analysis.R
@@ -537,6 +537,7 @@ plotPopulationSensitivity <- function(structureSet,
         title = paste("Population sensitivity of", pk, "of", op),
         plotConfiguration = settings$plotConfiguration
       )
+      op<-paste(strsplit(op,split = "|",fixed = TRUE)[[1]],collapse = "-")
       plotList[["plots"]][[paste(pk, op, sep = "-")]] <- plotObject
     }
   }
