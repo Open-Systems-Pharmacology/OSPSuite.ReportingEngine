8e606ced33c25fec9f0becf87ecebfde3a09e7ea
Unknown <chelle.pierre@gmail.com>
2020-05-07 06:15:47 -0400

    Add test for demography task workflows

---
+++ tests/dev/03_Population_Pediatric_Raltegavir/test-example-03-demography.R
@@ -0,0 +1,57 @@
+#------------------------------
+# Advanced example 03: Demography parameters
+#------------------------------
+library(ospsuite.reportingengine)
+library(ospsuite)
+
+output <- Output$new(path = "Organism|PeripheralVenousBlood|Raltegravir|Plasma (Peripheral Venous Blood)")
+
+popModelSets <- list(
+  Larson = PopulationSimulationSet$new(
+    referencePopulation = FALSE,
+    simulationSetName = "Larson",
+    simulationFile = "PKML/Larson 2013 8-18y meal.pkml",
+    populationFile = "Larson 2013 8-18y meal-Population.csv",
+    outputs = output
+  ),
+  Adult = PopulationSimulationSet$new(
+    referencePopulation = TRUE,
+    simulationSetName = "Adults",
+    simulationFile = "PKML/Raltegravir 400mg filmcoated tablet.pkml",
+    populationFile = "Raltegravir Adult Population.csv",
+    outputs = output
+  )
+)
+
+# Optional input: workflowType: parallelComparison (default), pediatric or ratioComparison
+mySimpleWorkflow <- PopulationWorkflow$new(
+  simulationSets = popModelSets,
+  workflowFolder = "myTestFolder"
+)
+
+# If results were not already exported in myTestFolder/PKAnalysisResults
+# you need to activate and run the task "populationPKParameters"
+mySimpleWorkflow$activateTasks(tasks = "plotDemography")
+mySimpleWorkflow$inactivateTasks(tasks = "simulatePopulation")
+
+# For pediatric, check that one population was set as reference population with referencePopulation = TRUE
+myPediatricWorkflow <- PopulationWorkflow$new(
+  workflowType = PopulationWorkflowTypes$pediatric,
+  simulationSets = popModelSets,
+  workflowFolder = "myTestFolder"
+)
+
+# If results were not already exported in myTestFolder/PKAnalysisResults
+# you need to activate and run the task "populationPKParameters"
+myPediatricWorkflow$activateTasks(tasks = "plotDemography")
+myPediatricWorkflow$inactivateTasks(tasks = "simulatePopulation")
+
+# Task "plotPKParameters": has xParameters and yParameters options
+# - xParameters are path of demography parameters for VPC like range plots (use ospsuite::StandardPath)
+# If NULL, only histograms will be output
+# - yParameters are selected PK parameters to be exported in output (use ospsuite::StandardPKParameter)
+
+myPediatricWorkflow$plotDemography$xParameters <- NULL
+myPediatricWorkflow$plotDemography$yParameters <- ospsuite::StandardPath$Weight
+
+myPediatricWorkflow$runWorkflow()
