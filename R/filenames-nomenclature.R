413faf8b0d544ab5554277cd4b21ea3ed5a1815b
Unknown <chelle.pierre@gmail.com>
2020-06-17 04:36:25 -0400

    Fixes #236 remove root dir of workflow from data source caption

--- R/filenames-nomenclature.R
+++ R/filenames-nomenclature.R
@@ -132,45 +132,49 @@ getGoodnessOfFitCaptions <- function(structureSet, plotType, plotScale = "linear
   if (plotScale %in% "log") {
     plotScale <- "logarithmic"
   }
+  dataSourceText <- ""
+  if(!is.null(structureSet$simulationSet$observedDataFile)){
+    workflowSubDir <- sub(pattern = "^.*[/]", replacement = "", x = structureSet$workflowFolder)
+    workflowRootDir <- sub(pattern = workflowSubDir, replacement = "", x = structureSet$workflowFolder)
+    
+    dataSourcePath <- sub(pattern = workflowRootDir, replacement = "", x = structureSet$simulationSet$observedDataFile)
+    dataSourceText <- paste0(". Data source: ", dataSourcePath))}
 
   if (plotType %in% "timeProfile") {
     return(paste0(
       "Time profiles of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)),
-      ". Time profiles are plotted in a ", plotScale, " scale."
+      dataSourceText, ". Time profiles are plotted in a ", plotScale, " scale."
     ))
   }
   if (plotType %in% "obsVsPred") {
     return(paste0(
       "Predicted vs observed of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)),
-      ". Predictions and observations are plotted in a ", plotScale, " scale."
+      dataSourceText, ". Predictions and observations are plotted in a ", plotScale, " scale."
     ))
   }
 
   if (plotType %in% "resVsPred") {
     return(paste0(
       "Logarithmic residuals vs predicted values of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)), "."
+      dataSourceText, "."
     ))
   }
   if (plotType %in% "resVsTime") {
     return(paste0(
       "Logarithmic residuals vs time of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)), "."
+      dataSourceText, "."
     ))
   }
   if (plotType %in% "resHisto") {
     return(paste0(
       "Logarithmic residuals distribution of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)), "."
+      dataSourceText, "."
     ))
   }
   if (plotType %in% "resQQPlot") {
     return(paste0(
       "Logarithmic residuals of ", structureSet$simulationSet$simulationSetName, " for ", structureSet$simulationSet$simulationName,
-      ifnotnull(structureSet$simulationSet$observedDataFile, paste0(". Data source: ", structureSet$simulationSet$observedDataFile)), 
-      " as quantile-quantile plot."
+      " as quantile-quantile plot", dataSourceText, "."
     ))
   }
 }
