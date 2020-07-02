508beaf351df26ea59fa1677745917ea8db88d14
Abdullah <aoh123@gmail.com>
2020-07-02 01:45:56 -0400

    test bug source

--- R/utilities-tracelib.R
+++ R/utilities-tracelib.R
@@ -1,38 +1,39 @@
 tracelibCheck <- function() {
+  return(FALSE)
   return(suppressWarnings(expr = require("tracelib", quietly = TRUE)))
 }
 
 re.tStartMetadataCapture <- function(...) {
   print("tStartMetadataCapture")
   if (tracelibCheck()) {
-    return(tracelib::tStartMetadataCapture(..., offset = 2))
+    #return(tracelib::tStartMetadataCapture(..., offset = 2))
   }
 }
 
 re.tEndMetadataCapture <- function(..., actionToken) {
   print("tEndMetadataCapture")
   if (tracelibCheck()) {
-    tracelib::tEndMetadataCapture(..., actionToken = actionToken)
+    #tracelib::tEndMetadataCapture(..., actionToken = actionToken)
   }
 }
 
 re.tStoreFileMetadata <- function(...) {
   print("tStoreFileMetadata")
   if (tracelibCheck()) {
-    tracelib::tStoreFileMetadata(...)
+    #tracelib::tStoreFileMetadata(...)
   }
 }
 
 re.tStartAction <- function(...) {
   print("tStartAction")
   if (tracelibCheck()) {
-    return(tracelib::tStartAction(..., offset = 1))
+    #return(tracelib::tStartAction(..., offset = 1))
   }
 }
 
 re.tEndAction <- function(..., actionToken) {
   print("tEndAction")
   if (tracelibCheck()) {
-    tracelib::tEndAction(..., actionToken = actionToken)
+    #tracelib::tEndAction(..., actionToken = actionToken)
   }
 }
