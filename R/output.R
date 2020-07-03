adef7a3a5c9b6f8c2da04afc0336a0d016548bb1
Unknown <chelle.pierre@gmail.com>
2020-07-03 17:23:32 -0400

    Fixes #253 dataSelection replaces dataFilter

--- R/output.R
+++ R/output.R
@@ -1,9 +1,15 @@
+#' @export
+DataSelectionKeys <- list(
+  NONE = "NONE",
+  ALL = "ALL"
+)
+
 #' @title Output
 #' @description R6 class representing workflow output
 #' @field path path name for the simulation (e.g. `Organism|PeripheralVenousBlood|Raltegravir|Plasma (Peripheral Venous Blood)`)
 #' @field displayName display name for `path`
 #' @field displayUnit display unit for `path`
-#' @field dataFilter character or expression used to filter the observed data
+#' @field dataSelection character or expression used to filter the observed data
 #' @field dataDisplayName display name of the observed data
 #' @field pkParameters R6 class `PkParameterInfo` objects
 #' @export
@@ -15,7 +21,7 @@ Output <- R6::R6Class(
     path = NULL,
     displayName = NULL,
     displayUnit = NULL,
-    dataFilter = NULL,
+    dataSelection = NULL,
     dataDisplayName = NULL,
     pkParameters = NULL,
 
@@ -24,24 +30,24 @@ Output <- R6::R6Class(
     #' @param path path name for the simulation (e.g. `Organism|PeripheralVenousBlood|Raltegravir|Plasma (Peripheral Venous Blood)`)
     #' @param displayName display name for `path`
     #' @param displayUnit display unit for `path`
-    #' @param dataFilter characters or expression to filter the observed data
+    #' @param dataSelection characters or expression to filter the observed data
     #' @param dataDisplayName display name of the observed data
     #' @param pkParameters R6 class `PkParameterInfo` objects or their names
     #' @return A new `Output` object
     initialize = function(path,
-                          displayName = NULL,
-                          displayUnit = NULL,
-                          dataFilter = NULL,
-                          dataDisplayName = NULL,
-                          pkParameters = NULL) {
+                              displayName = NULL,
+                              displayUnit = NULL,
+                              dataSelection = DataSelectionKeys$NONE,
+                              dataDisplayName = NULL,
+                              pkParameters = NULL) {
       validateIsString(path)
       validateIsOfLength(path, 1)
       validateIsString(c(displayName, displayUnit, dataDisplayName), nullAllowed = TRUE)
       ifnotnull(displayName, validateIsOfLength(displayName, 1))
       ifnotnull(displayUnit, validateIsOfLength(displayUnit, 1))
       ifnotnull(dataDisplayName, validateIsOfLength(dataDisplayName, 1))
-      validateIsOfType(dataFilter, c("character", "expression"), nullAllowed = TRUE)
-      ifnotnull(dataFilter, validateIsOfLength(dataFilter, 1))
+      validateIsOfType(dataSelection, c("character", "expression"), nullAllowed = TRUE)
+      ifnotnull(dataSelection, validateIsOfLength(dataSelection, 1))
       validateIsOfType(c(pkParameters), c("character", "PkParameterInfo"), nullAllowed = TRUE)
 
       self$path <- path
@@ -49,10 +55,16 @@ Output <- R6::R6Class(
       self$displayUnit <- displayUnit
 
       # If filter is null, assumes that user won't get any observed data
-      self$dataFilter <- dataFilter
+      self$dataSelection <- dataSelection
       # Ensure that dataFilter is of type expression
-      if (isOfType(self$dataFilter, "character")) {
-        self$dataFilter <- parse(text = self$dataFilter)
+      if (isOfType(self$dataSelection, "character")) {
+        if (self$dataSelection %in% DataSelectionKeys$ALL) {
+          self$dataSelection <- "TRUE"
+        }
+        self$dataSelection <- parse(text = self$dataSelection)
+        if (dataSelection %in% DataSelectionKeys$NONE) {
+          self$dataSelection <- NULL
+        }
       }
 
       self$dataDisplayName <- dataDisplayName %||% paste0(self$displayName, " observed data")
