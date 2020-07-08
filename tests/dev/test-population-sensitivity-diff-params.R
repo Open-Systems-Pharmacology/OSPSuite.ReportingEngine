72d8bacfd92be767c05bd42568f026922fe3d834
Abdullah <aoh123@gmail.com>
2020-07-08 03:28:29 -0400

    update examples

--- tests/dev/test-population-sensitivity-diff-params.R
+++ tests/dev/test-population-sensitivity-diff-params.R
@@ -2,7 +2,6 @@ rm(list = ls())
 library(ospsuite)
 graphics.off()
 library(ospsuite.reportingengine)
-# devtools::load_all("C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine")
 simulationFile1 <- "./tests/dev/popdiffsens/RaltegravirSim.pkml"
 simulationFile2 <- "./tests/dev/popdiffsens/LarsonSim.pkml"
 populationFile1 <- "./tests/dev/popdiffsens/RalPop10.csv"
@@ -45,6 +44,6 @@ pwf$populationSensitivityAnalysis$settings$variableParameterPaths <- c(
   "Applications|Iwamoto 2008 400mg PO (Figure 1) omeprazole study|filmcoated tablet (original Merck formulation)|Application_1|ProtocolSchemaItem|Dose"
 )
 pwf$populationSensitivityAnalysis$settings$quantileVec <- c(0.5)
-pwf$plotSensitivity$settings <- SensitivityPlotSettings$new(totalSensitivityThreshold = 1, maximalParametersPerSensitivityPlot = 12, plotFontSize = 6)
+pwf$plotSensitivity$settings <- SensitivityPlotSettings$new(totalSensitivityThreshold = 1, maximalParametersPerSensitivityPlot = 12)
 
 pwf$runWorkflow()
