8407d9b8954bcd006b8e36c75599471a6520e91f
Unknown <chelle.pierre@gmail.com>
2020-10-08 08:56:21 -0400

    Use new excel template dedicated to R version

--- tests/dev/test-excel-template.R
+++ tests/dev/test-excel-template.R
@@ -1,20 +1,16 @@
 # Test script to create workflow.R from an Excel template
 
-# Two Excel templates are available in the extdata folder
+# An Excel template is available in the extdata folder
 # The best practice is to copy and update them, file.copy() can be used for that purpose
 
-# Template 1: Mean Model workflow
-excelFile <- system.file("extdata", "mean-model-workflow-input.xlsx", package = "ospsuite.reportingengine")
-
-# Template 2: Population model workflow
-# excelFile <- system.file("extdata", "population-workflow-input.xlsx", package = "ospsuite.reportingengine")
+# Template is available at the following path
+excelFile <- system.file("extdata", "WorkflowInput.xlsx", package = "ospsuite.reportingengine")
 
 workflowFile <- createWorkflowFromExcelInput(excelFile)
 
-# Two optional parameters can be added:
+# An optional parameter can be added:
 # workflowFile: name of the R script file created from the Excel file
-# workflowFolder: name of the folder in which the wrokflow results will be stored
-# createWorkflowFromExcelInput(excelFile, workflowFile = "workflow.R", workflowFolder = "Output")
+# createWorkflowFromExcelInput(excelFile, workflowFile = "workflow.R")
 
 # Run the resulting file:
 source(workflowFile, encoding = 'UTF-8')
