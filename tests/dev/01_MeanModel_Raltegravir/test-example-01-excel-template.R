d75e89eec4d4380e769852f0c54e77234380f93d
Unknown <chelle.pierre@gmail.com>
2020-11-03 10:14:48 -0500

    Fixes #344 Update the test script

--- tests/dev/01_MeanModel_Raltegravir/test-example-01-excel-template.R
+++ tests/dev/01_MeanModel_Raltegravir/test-example-01-excel-template.R
@@ -1,12 +1,12 @@
 # Test script to create workflow.R from an Excel template
 
-setwd("C:/Design2Code/OSPSuite.ReportingEngine/tests/dev/01_MeanModel_Raltegravir")
 excelFile <- "workflow-input-01.xlsx"
 
 workflowFile <- createWorkflowFromExcelInput(excelFile)
 
-# An optional parameter can be added:
+# Optional parameters can be added:
 # workflowFile: name of the R script file created from the Excel file
+# removeComments: logical to remove all comments provided as guidance to review the script
 # createWorkflowFromExcelInput(excelFile, workflowFile = "workflow.R")
 
 # Run the resulting file:
