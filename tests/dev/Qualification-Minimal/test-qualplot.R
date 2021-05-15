32e13eda562621dbf70bc958d41539e691a8c101
Abdullah Hamadeh <aoh123@gmail.com>
2021-05-15 12:57:43 -0400

    Update test-workflow.R

---
+++ tests/dev/Qualification-Minimal/test-qualplot.R
@@ -0,0 +1,32 @@
+# library(ospsuite.reportingengine)
+rm(list = ls())
+devtools::load_all(".")
+#-------- Qualification Inputs --------#
+# Configuration Plan is stored within a json file
+configurationFile <- "tests/dev/Qualification-Minimal/reporting engine input/report-configuration-plan.json"
+workflowFolder <- "tests/dev/Qualification-Minimal/reporting engine output"
+
+#-------- Workflow Definition --------#
+workflow <- loadQualificationWorkflow(
+  workflowFolder = workflowFolder,
+  jsonFile = configurationFile
+)
+
+
+#-------- Configuration Plan --------#
+configurationPlan <- loadConfigurationPlan(
+  workflowFolder = workflowFolder,
+  jsonFile = configurationFile
+)
+
+schedule <- getScheduleOfPlots(configurationPlan)
+
+outputsTimeProfile1 <- getOutputsTimeProfile(plot = configurationPlan$plots$TimeProfile[[1]])
+outputsTimeProfile2 <- getOutputsTimeProfile(plot = configurationPlan$plots$TimeProfile[[2]])
+outputsTimeProfile3 <- getOutputsTimeProfile(plot = configurationPlan$plots$TimeProfile[[3]])
+
+
+outputsGOF <- getOutputsGOFMergedPlotsPlot(plot = configurationPlan$plots$GOFMergedPlots[[1]])
+
+
+gofTaskSettings <- GofTaskSettings$new(taskName = "plotTimeProfilesAndResiduals",outputsTimeProfile1)
