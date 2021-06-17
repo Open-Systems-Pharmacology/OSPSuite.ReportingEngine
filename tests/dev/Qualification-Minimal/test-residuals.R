68d766cbbb3ce83bd30959b2ca6cbbe0e8bf9b10
Abdullah Hamadeh <aoh123@gmail.com>
2021-06-16 23:18:13 -0400

    Update test-residuals.R

--- tests/dev/Qualification-Minimal/test-residuals.R
+++ tests/dev/Qualification-Minimal/test-residuals.R
@@ -3,7 +3,15 @@ rm(list = ls())
 library(ospsuite)
 setwd(dir = "C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine/")
 devtools::load_all(".")
-setwd(dir = "tests/dev/Qualification-Minimal")
+
+
+re.example.dir <- "tests/dev/Qualification-Minimal"
+minimal.example.dir <-"../QualificationPlan/examples/minimal"
+advanced.example.dir <-"../QualificationPlan/examples/advanced_01/"
+example.dirs <- c(re.example.dir,minimal.example.dir,advanced.example.dir)
+example.number <- 3
+
+setwd(dir = example.dirs[example.number])
 
 #-------- Qualification Inputs --------#
 # Configuration Plan is stored within a json file
@@ -12,12 +20,13 @@ inputFolder <- "reporting engine input"
 configurationFile <- file.path(inputFolder, "/report-configuration-plan.json")
 workflowFolder <- "reporting engine output"
 
-#-------- Workflow Definition --------#
+# #-------- Workflow Definition --------#
 # workflow <- loadQualificationWorkflow(
 #   workflowFolder = workflowFolder,
 #   configurationPlanFile = configurationFile
 # )
-
+# # Workflow works as mean model and population workflows
+# workflow$runWorkflow()
 
 #-------- Configuration Plan --------#
 configurationPlan <- loadConfigurationPlan(
