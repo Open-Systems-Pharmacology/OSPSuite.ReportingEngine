fe01ccdce5b843ca6c99aa44c9d954eff6cc7ca3
Abdullah Hamadeh <aoh123@gmail.com>
2021-06-29 14:58:40 -0400

    Update test-residuals.R

--- tests/dev/Qualification-Minimal/test-residuals.R
+++ tests/dev/Qualification-Minimal/test-residuals.R
@@ -10,7 +10,7 @@ re.example.dir <- "tests/dev/Qualification-Minimal"
 minimal.example.dir <- "../QualificationPlan/examples/minimal"
 advanced.example.dir <- "../QualificationPlan/examples/advanced_01/"
 example.dirs <- c(re.example.dir, minimal.example.dir, advanced.example.dir)
-example.number <- 1
+example.number <- 3
 
 setwd(dir = example.dirs[example.number])
 
@@ -18,7 +18,7 @@ setwd(dir = example.dirs[example.number])
 # Configuration Plan is stored within a json file
 
 inputFolder <- "reporting engine input"
-configurationFile <- file.path(inputFolder, "/report-configuration-plan.json")
+configurationFile <- file.path(inputFolder, "small-report-configuration-plan.json")
 workflowFolder <- "reporting engine output"
 
 # #-------- Workflow Definition --------#
@@ -26,6 +26,9 @@ workflow <- loadQualificationWorkflow(
   workflowFolder = workflowFolder,
   configurationPlanFile = configurationFile
 )
+
+workflow$inactivateTasks("simulate")
+workflow$inactivateTasks("plotTimeProfiles")
 # # Workflow works as mean model and population workflows
 workflow$runWorkflow()
 
