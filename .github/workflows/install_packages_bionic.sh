b3ace5e3faac5d23fdc6b95bb7763ff55e78a5fc
Juri Solodenko <Yuri05@users.noreply.github.com>
2021-07-09 16:30:40 +0200

    Linux tests (#503)
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    * Run RE Tests on Linux
    
    Co-authored-by: Yuri05 <Yuri05@github.com>

---
+++ .github/workflows/install_packages_bionic.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Get software 
+wget -nv https://ci.appveyor.com/api/buildjobs/9qcwkyuo2orotq0r/artifacts/ospsuite_10.0.39_ubuntu18.tar.gz -P /tmp_setup/
+
+# Install packages
+R --no-save -e "install.packages('remotes', repos='http://cran.rstudio.com/'); remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
+R CMD INSTALL /tmp_setup/ospsuite_10.0.39_ubuntu18.tar.gz --install-tests
---
+++ .github/workflows/testRE.R
@@ -0,0 +1,7 @@
+#options(error = function() {print(rlang::trace_back(bottom = sys.frame(-1))) })
+library('ospsuite')
+#files.sources = list.files(path='R', pattern = '*.R', full.names = TRUE)
+#sapply(files.sources, source)
+setwd('tests')
+library(testthat)
+test_local(reporter = "summary")
