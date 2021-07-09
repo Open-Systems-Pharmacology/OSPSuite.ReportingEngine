0d8e7534146198a931a839d9e46b9ebcbbf97e5b
Yuri05 <Yuri05@github.com>
2021-07-09 12:28:40 +0200

    Run RE Tests on Linux

--- .github/workflows/install_packages_centos7.sh
+++ .github/workflows/install_packages_centos7.sh
@@ -4,5 +4,5 @@
 wget -nv https://ci.appveyor.com/api/buildjobs/9qcwkyuo2orotq0r/artifacts/ospsuite_10.0.39_centOS7.tar.gz -P /tmp_setup/
 
 # Install packages
-R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
+R --no-save -e "install.packages('remotes', repos='http://cran.rstudio.com/');remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
 R CMD INSTALL /tmp_setup/ospsuite_10.0.39_centOS7.tar.gz --install-tests
