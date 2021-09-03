ef51c37f24c04c143b533abd29fbce228c09f26d
Abdullah Hamadeh <aoh123@gmail.com>
2021-09-03 07:33:44 -0400

    Update qualification-ddi.R

--- R/qualification-ddi.R
+++ R/qualification-ddi.R
@@ -12,7 +12,14 @@ getQualificationDDIPlotData <- function(configurationPlan){
     plotDDIMetadata$title <- plot$Title
     plotDDIMetadata$sectionID <- plot$SectionId
     # Pipes in configuration plan will be deprecated moving forward
+    #browser()
     plotDDIMetadata$plotTypes <- plot$PlotTypes %||% ospsuite::toPathArray(plot$PlotType)
+    plotDDIMetadata$axesSettings <- lapply(plotDDIMetadata$plotTypes, function(pltType) {
+      getAxesSettings(configurationPlan$plots$AxesSettings[[ddiPlotAxesSettings[[pltType]]]])
+    })
+    names(plotDDIMetadata$axesSettings) <- plotDDIMetadata$plotTypes
+
+
     plotDDIMetadata$groups <- list()
 
     pkParameters <- NULL
@@ -105,3 +112,92 @@ getQualificationDDIPlotData <- function(configurationPlan){
 
 
 
+
+
+
+
+
+#' @title buildQualificationDDIPredictedVsObserved
+#' @description Build dataframe for observation vs prediction
+#' @param dataframe data.frame
+#' @param metadata meta data on `data`
+#' @return dataframe for plotting goodness of fit of predictedVsObserved type
+buildQualificationDDIPredictedVsObserved <- function(dataframe,
+                                                     metadata) {
+  axesSettings <- metadata$axesSettings[["predictedVsObserved"]]
+
+  xUnit <- axesSettings$X$unit
+  xDimension <- axesSettings$X$dimension
+  xScaling <- axesSettings$X$scaling
+  xGridlines <- axesSettings$X$gridLines
+
+  yUnit <- axesSettings$Y$unit
+  yDimension <- axesSettings$Y$dimension
+  yScaling <- axesSettings$Y$scaling
+  yGridlines <- axesSettings$Y$gridLines
+
+  # function to do obs vs sim
+  ddiPlotDataframe <- NULL
+  aestheticsList <- list(shape = list(), color = list())
+  uniqueGroupPKParameterID <- 0
+  for (grp in unique(dataframe$groupNumber)) {
+    aestheticsList$shape[[grp]] <- metadata$groups[[grp]]$symbol
+    aestheticsList$color[[grp]] <- metadata$groups[[grp]]$color
+
+    for (pkParameter in unique(dataframe[dataframe$group == grp, ]$pkParameter )) {
+
+      uniqueGroupPKParameterID <- uniqueGroupPKParameterID + 1
+
+      xDataDimension <- ""
+      xDataUnit <- ""
+      xData <- dataframe[dataframe$group == grp & dataframe$pkParameter == pkParameter, ]$observedRatio
+
+      if (xScaling == "Log") {
+        xData <- log10(xData)
+      }
+      xData <- replaceInfWithNA(xData)
+
+      yDataDimension <- ""
+      yDataUnit <- ""
+      yData <- dataframe[dataframe$group == grp & dataframe$pkParameter == pkParameter, ]$simulatedRatio
+      if (yScaling == "Log") {
+        yData <- log10(yData)
+      }
+      yData <- replaceInfWithNA(yData)
+
+      df <- data.frame(
+        Observed = xData,
+        Simulated = yData,
+        Group = grp,
+        pkParameter = pkParameter,
+        uniqueGroupPKParameterID = uniqueGroupPKParameterID
+      )
+
+      df$uniqueGroupPKParameterID <- as.factor(df$uniqueGroupPKParameterID)
+
+      ddiPlotDataframe <- rbind.data.frame(ddiPlotDataframe, df)
+    }
+  }
+  return(list(ddiPlotDataframe = ddiPlotDataframe, aestheticsList = aestheticsList, axesSettings = axesSettings))
+}
+#'
+#'
+#'
+#'
+#' Names of fields in configuration plane containing axes settings data for each GOF plot type
+ddiPlotAxesSettings <- list(
+  "predictedVsObserved" = "DDIRatioPlotsPredictedVsObserved",
+  "residualsVsObserved" = "DDIRatioPlotsResidualsVsObserved"
+)
+
+#' #' Names of functions for extracting data for each DDI plot type
+#' buildDDIDataFrameFunctions <- list(
+#'   "predictedVsObserved" = buildQualificationDDIPredictedVsObserved,
+#'   "residualsOverTime" = buildQualificationDDIResidualsVsObserved
+#' )
+#'
+#' #' Names of functions for plotting DDI plots for each DDI plot type
+#' plotDDIFunctions <- list(
+#'   "predictedVsObserved" = plotQualificationDDIPredictedVsObserved,
+#'   "residualsOverTime" = plotQualificationDDIResidualsVsObserved
+#' )
