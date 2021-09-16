b313b97db22eab5b94f5895f6e6c61ac8e7afc3e
Yuri05 <Yuri05@github.com>
2021-09-16 19:25:30 +0200

    Fixes #545 Update ospsuite-R version for Linux builds

--- .github/workflows/install_packages_centos7.sh
+++ .github/workflows/install_packages_centos7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Get software 
-wget -nv https://ci.appveyor.com/api/buildjobs/9qcwkyuo2orotq0r/artifacts/ospsuite_10.0.39_centOS7.tar.gz -P /tmp_setup/
+wget -nv https://ci.appveyor.com/api/buildjobs/byfuom8t9v6735v3/artifacts/ospsuite_10.0.63_centOS7.tar.gz -P /tmp_setup/
 
 # Install packages
 R --no-save -e "install.packages('remotes', repos='http://cran.rstudio.com/');remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
-R CMD INSTALL /tmp_setup/ospsuite_10.0.39_centOS7.tar.gz --install-tests
+R CMD INSTALL /tmp_setup/ospsuite_10.0.63_centOS7.tar.gz --install-tests
