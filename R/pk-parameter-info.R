42d2655975cc982b44b71dd7994a979390a12a4b
Unknown <chelle.pierre@gmail.com>
2021-11-10 03:47:54 -0500

    group as last argument to prevent break when unnamed arguments are used

--- R/pk-parameter-info.R
+++ R/pk-parameter-info.R
@@ -1,32 +1,32 @@
 #' @title PkParameterInfo
 #' @description R6 class representing workflow selected PK parameter.
 #' @field pkParameter PK Parameter name for the simulation (e.g. `AUC_inf`)
-#' @field group Grouping identifier
 #' @field displayName display name for `pkParameter`
 #' @field displayUnit display unit for `pkParameter`
+#' @field group Grouping identifier
 #' @export
 #' @import ospsuite
 PkParameterInfo <- R6::R6Class(
   "PkParameterInfo",
   cloneable = FALSE,
   public = list(
-    group = NULL,
     pkParameter = NULL,
     displayName = NULL,
     displayUnit = NULL,
+    group = NULL,
 
     #' @description
     #' Create a new `PkParameterInfo` object.
     #' @param pkParameter PK Parameter name for the simulation (e.g. `AUC_inf`)
     #' Tips: use ospsuite::allPKParameterNames() to get a list of available PK parameters
-    #' @param group Grouping identifier. Default group associate `pkParameter`.
     #' @param displayName display name for `pkParameter`
     #' @param displayUnit display unit for `pkParameter`
+    #' @param group Grouping identifier. Default group associate `pkParameter`.
     #' @return A new `PkParameterInfo` object
     initialize = function(pkParameter,
-                          group = NULL,
                           displayName = NULL,
-                          displayUnit = NULL) {
+                          displayUnit = NULL,
+                          group = NULL) {
       validateIsString(pkParameter)
       validateIsOfLength(pkParameter, 1)
       validateIsString(c(displayName, displayUnit), nullAllowed = TRUE)
