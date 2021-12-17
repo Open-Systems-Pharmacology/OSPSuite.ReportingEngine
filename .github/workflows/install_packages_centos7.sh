7003cf8822ecf6b4479c3030832f848ceefa7a16
Yuri05 <Yuri05@github.com>
2021-12-17 11:38:41 +0100

    Fixes #648 Always use the latest ospsuite version in builds

--- .github/workflows/install_packages_centos7.sh
+++ .github/workflows/install_packages_centos7.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Get software 
-wget -nv https://ci.appveyor.com/api/buildjobs/byfuom8t9v6735v3/artifacts/ospsuite_10.0.63_centOS7.tar.gz -P /tmp_setup/
+wget -nv https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-r/artifacts/ospsuite_centOS7.tar.gz -P /tmp_setup/
 
 # Install packages
-R --no-save -e "install.packages('remotes', repos='http://cran.rstudio.com/');remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
-R CMD INSTALL /tmp_setup/ospsuite_10.0.63_centOS7.tar.gz --install-tests
+R --no-save -e "install.packages('remotes', repos='http://cran.rstudio.com/'); remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop'); remotes::install_github('Open-Systems-Pharmacology/OSPSuite.RUtils', ref ='main')"
+R CMD INSTALL /tmp_setup/ospsuite_centOS7.tar.gz --install-tests
