b687c0f496eb873fffb9db9db3fa50550156e570
Unknown <chelle.pierre@gmail.com>
2022-04-07 10:08:30 -0400

    Fixes #772 Add patchwork required by ospsuite to linux environment

--- .github/workflows/install_packages_cran.sh
+++ .github/workflows/install_packages_cran.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Install packages
-R --no-save -e "install.packages(c('remotes', 'dplyr', 'tidyr', 'purrr', 'ospsuite.utils'), repos='http://cran.rstudio.com/')"
+R --no-save -e "install.packages(c('remotes', 'dplyr', 'tidyr', 'purrr', 'ospsuite.utils', 'patchwork'), repos='http://cran.rstudio.com/')"
 R --no-save -e "remotes::install_github('Open-Systems-Pharmacology/TLF-Library', ref ='develop')"
