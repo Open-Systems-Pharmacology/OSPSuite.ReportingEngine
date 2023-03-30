962d7a0e43da6f21ac4d62f3ca2e98e75b4b6599
Unknown <chelle.pierre@gmail.com>
2023-03-30 03:01:58 -0400

    Remove 0-fold from legend

--- R/qualification-gof.R
+++ R/qualification-gof.R
@@ -257,8 +257,8 @@ getQualificationGOFPlot <- function(plotType, data, metaData, axesProperties, pl
     )
   )
   gofPlot <- updatePlotAxes(gofPlot, axesProperties)
-  # Remove legend from colors
-  gofPlot <- gofPlot + ggplot2::guides(color = "none")
+  # Remove color and line from legend
+  gofPlot <- gofPlot + ggplot2::guides(color = "none", linetype = "none")
   return(gofPlot)
 }
 
