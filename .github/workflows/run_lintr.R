cdf41a02c407cbfaaf9491e22a16db3da49f67d4
pchelle <chelle.pierre@gmail.com>
2023-12-14 04:45:59 -0500

    Change ways to install and run lintr

---
+++ .github/workflows/run_lintr.R
@@ -0,0 +1,13 @@
+lintr::lint_package(
+  linters=lintr::linters_with_defaults(
+    object_name_linter=lintr::object_name_linter(styles=c("camelCase","CamelCase","symbols")),
+    line_length_linter=NULL, 
+    object_length_linter=NULL, 
+    brace_linter=NULL, 
+    paren_body_linter=NULL, 
+    trailing_whitespace_linter=NULL, 
+    trailing_blank_lines_linter=NULL, 
+    spaces_left_parentheses_linter=NULL, 
+    infix_spaces_linter=NULL
+  )
+)
